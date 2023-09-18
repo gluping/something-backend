93175a5caa62e0d37efa2ebf5bf11f30748994eb
Sreejith(Dikru) <imsreejithofficial@gmail.com>
2023-09-18 20:48:11 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -4,13 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# Association Table to link users and rooms in a many-to-many relationship
-room_participants = Table(
-    "room_participants",
-    Base.metadata,
-    Column("room_id", Integer, ForeignKey("rooms.id")),
-    Column("user_id", Integer, ForeignKey("users.id"))
-)
 
 class User(Base):
     __tablename__ = "users"
@@ -19,27 +12,4 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
-    # Establishing the many-to-many relationship with Room model
-    joined_rooms = relationship("Room", secondary=room_participants, back_populates="participants")
-    messages = relationship("Message", back_populates="user")
-class Room(Base):
-    __tablename__ = "rooms"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
-    current_participants = Column(Integer, default=0)  # New field to track current participants
-
-    # Establishing the many-to-many relationship with User model
-    participants = relationship("User", secondary=room_participants, back_populates="joined_rooms")
-    messages = relationship("Message", back_populates="room")
-
-class Message(Base):
-    __tablename__ = "messages"
-    id = Column(Integer, primary_key=True, index=True)
-    text = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    room_id = Column(Integer, ForeignKey("rooms.id"))
-
-    user = relationship("User", back_populates="messages")
-    room = relationship("Room", back_populates="messages")
+  
