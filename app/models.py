1022a1a30d09c28cbc83f7f48eded37d2707e83f
sreejith <imsreejithofficial@gmail.com>
2023-11-25 12:26:17 +0530

    patch(bugs

--- app/models.py
+++ app/models.py
@@ -20,28 +20,52 @@ class ActivityProvider(Base):
     contact_email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    activities = relationship("Activity", back_populates = "provider")
+    activities = relationship("Activity", back_populates="provider")
 
 class Activity(Base):
-    __tablename__="activity"
-    id = Column(Integer,primary_key=True, nullable=False)
+    __tablename__="activities"  # Corrected table name
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     location = Column(String, nullable=False)
     price = Column(Float, nullable=False)
     image_url = Column(String)  # Store the URL of the uploaded image
 
-    provider_id = Column(Integer, ForeignKey("activity_providers.id"), nullable = False)
-    provider = relationship("ActivityProvider", back_populates = "activities")
+    provider_id = Column(Integer, ForeignKey("activity_providers.id"), nullable=False)
+    provider = relationship("ActivityProvider", back_populates="activities")
 
-    time_slots = relationship("TimeSlot", back_populates="activity")
+    # time_slots = relationship("TimeSlot", back_populates="activities")
 
 
-class TimeSlot(Base):
-    __tablename__ = "time_slots"
-    id = Column(Integer, primary_key=True, nullable=False)
-    start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=False)
-    activity_id = Column(Integer, ForeignKey("activity.id"), nullable=False)
-    is_available = Column(Boolean, nullable=False, default=True)
+# class TimeSlot(Base):
+#     __tablename__ = "time_slots"
+#     id = Column(Integer, primary_key=True, nullable=False)
+#     start_time = Column(DateTime, nullable=False)
+#     end_time = Column(DateTime, nullable=False)
+#     activity_id = Column(Integer, ForeignKey("activities.id"), nullable=False)  # Corrected foreign key
+#     is_available = Column(Boolean, nullable=False, default=True)
 
+# class Payment(Base):
+#     __tablename__ = "payments"
+#     id = Column(Integer, primary_key=True, nullable=False)
+#     amount = Column(Float, nullable=False)
+#     status = Column(String, nullable=False, default="Pending")
+#     booking_id = Column(Integer, ForeignKey("bookings.id"), nullable=False)
+#     booking = relationship("Booking", back_populates="payment")
+
+
+# class Booking(Base):
+#     __tablename__ = "bookings"
+#     id = Column(Integer, primary_key=True, nullable=False)
+#     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+#     activity_id = Column(Integer, ForeignKey("activities.id"), nullable=False)
+#     time_slot_id = Column(Integer, ForeignKey("time_slots.id"), nullable=False)
+#     payment_id = Column(Integer, ForeignKey("payments.id"), nullable=False)
+#     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+#     user = relationship("User", back_populates="bookings")
+#     activity = relationship("Activity", back_populates="bookings")
+#     time_slot = relationship("TimeSlot", back_populates="bookings")
+#     payment = relationship("Payment", back_populates="booking")
+
+    # You can add more columns based on your booking requirements
